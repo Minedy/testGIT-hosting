b9773cbead3e3dc2b9c78a114121594ecc41e7ae
Minedy <fatum@tut.by>
2019-10-26 21:33:36 +0300

    fogotten -m

---
+++ init.cpp
@@ -0,0 +1,9 @@
+hello world
+MemTotal:       12501168 kB
+MemFree:         8954908 kB
+HighTotal:             0 kB
+HighFree:              0 kB
+LowTotal:       12501168 kB
+LowFree:         8954908 kB
+SwapTotal:       1900544 kB
+SwapFree:        1897720 kB
